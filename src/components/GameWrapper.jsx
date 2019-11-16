b3dc64c208bf49959588268c7ecdde835faa30cf
JJ <justinjcode@gmail.com>
2019-11-16 10:40:53 -0800

    amplify

---
+++ src/components/GameWrapper.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class GameWrapper extends Component {
+  render() {
+      return (
+          <div>poop</div>
+      )
+  }
+}
+
+export default GameWrapper;
