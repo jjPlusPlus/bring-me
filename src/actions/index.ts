8544cee2d1c68764b7ba975c82c114eb64fdb60a
Francesca Reyes <francheesey@gmail.com>
2019-11-26 21:39:16 -0800

    Add game logic actions for setting up game and clock, updating clock, traversing through list of words, and updating score

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -7,16 +7,59 @@ export interface AddScreenshot {
   };
 }
 
-export interface RemoveScreenshot {
-  type: "REMOVE_SCREENSHOT";
-  payload?: {
-    data: string;
-  };
+export interface SetupGame {
+  type: "SETUP_GAME";
 }
 
-export type ApplicationActionTypes = AddScreenshot | RemoveScreenshot;
+export interface SetClock {
+  type: "SET_CLOCK";
+  payload: {
+    value: number
+  }
+}
+
+export interface DecrementClock {
+  type: "DECREMENT_CLOCK";
+}
+
+export interface NextWord {
+  type: "NEXT_WORD";
+}
+
+export interface IncreaseScore {
+  type: "INCREASE_SCORE";
+}
+
+export type ApplicationActionTypes = 
+  | AddScreenshot 
+  | SetupGame 
+  | SetClock
+  | DecrementClock
+  | NextWord
+  | IncreaseScore;
 
 export const addScreenshot = (data: string) => ({
   type: "ADD_SCREENSHOT",
   payload: data
 });
+
+export const setupGame = () => ({
+  type: "SETUP_GAME",
+})
+
+export const setClock = (value: number) => ({
+  type: "SET_CLOCK",
+  payload: { value },
+})
+
+export const nextWord = () => ({
+  type: "NEXT_WORD",
+})
+
+export const increaseScore = () => ({
+  type: "INCREASE_SCORE",
+})
+
+/*
+  is there a way not to have to repeat everything?
+*/
