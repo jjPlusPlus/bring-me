e746fd774f19cfbd3241661f8a08efecc0bbb912
Francesca Reyes <francheesey@gmail.com>
2019-11-26 21:40:22 -0800

    Create placeholder game views for score, list of words, current word, and timer; set up redux functionality

--- src/components/GameWrapper.tsx
+++ src/components/GameWrapper.tsx
@@ -1,14 +1,50 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import PlayerCamera from './PlayerCamera';
+import { connect } from 'react-redux';
+import { Dispatch, AnyAction } from 'redux';
+import { AppState } from '../types';
+import { setupGame, nextWord, increaseScore } from '../actions';
 
-class GameWrapper extends Component {
-  render() {
-      return (
-          <div>
-            <PlayerCamera />
-          </div>
-      )
-  }
+const GameWrapper: React.FC = ( props: any ) => {
+  useEffect(() => {
+    props.setupGame();
+  }, []); // jj says this is terrible.
+  console.log(props)
+  const currentWord = props.Game.words[props.Game.currentWordIndex].text;
+  return (
+    <div>
+      <div>Score: {props.Game.score}</div>
+      <div>Words: {props.Game.words.map((word: any, index: number) => {
+        return (
+          <span key={index}>{word.text} </span>
+        )
+      })}</div>
+      <div>Timer: {props.Game.timer}</div>
+      <h2>Current Word: {currentWord} </h2>
+      <button onClick={props.nextWord}>Skip</button>
+      <button onClick={props.increaseScore}>Correct</button>
+      <PlayerCamera />
+    </div>
+  )
 }
 
-export default GameWrapper;
+const mapStateToProps = (state: AppState) => ({
+  ...state,
+})
+
+const mapDispatchToProps = (dispatch: Dispatch<AnyAction>) => ({
+  setupGame: () => dispatch(setupGame()),
+  nextWord: () => dispatch(nextWord()),
+  increaseScore: () => dispatch(increaseScore()),
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(GameWrapper);
+
+/* 
+  TODO
+    1) Convert to function component ✔️
+    2) Connect component to Redux
+    3) When component loads, start game by firing off redux action
+
+    ask david fn()()()
+*/
