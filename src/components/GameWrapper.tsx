2ccde98e9e8d0847c7869843a2b324c864e9ccf1
Francesca Reyes <francheesey@gmail.com>
2019-11-27 22:43:09 -0800

    Start on game layout

--- src/components/GameWrapper.tsx
+++ src/components/GameWrapper.tsx
@@ -12,18 +12,35 @@ const GameWrapper: React.FC = ( props: any ) => {
   console.log(props)
   const currentWord = props.Game.words[props.Game.currentWordIndex].text;
   return (
-    <div>
-      <div>Score: {props.Game.score}</div>
-      <div>Words: {props.Game.words.map((word: any, index: number) => {
-        return (
-          <span key={index}>{word.text} </span>
-        )
-      })}</div>
-      <div>Timer: {props.Game.timer}</div>
-      <h2>Current Word: {currentWord} </h2>
+    <div className="flex flex-col">
+      <div className="w-full text-center text-3xl">{props.Game.timer}</div>
+      <div className="flex max-w-6xl w-full mx-auto">
+        <div className="words flex flex-col w-1/6 p-4 bg-gray-100 mx-2 rounded shadow">
+          {props.Game.words.map((word: any, index: number) => {
+            let check;
+            if (word.text === currentWord) {
+              check = true;
+            }
+            return (
+              <div>
+                { check ? <span>current -></span> : ""}
+                <span key={index}>{word.text} </span>
+              </div>
+            )
+          })}
+        </div>
+        <div className="w-2/3 flex flex-col justify-center items-center">
+          <h2>Current Word: {currentWord} </h2>
+          <PlayerCamera />
+        </div>
+        <div className="w-1/6">
+          <div>Score: {props.Game.score}</div>
+        </div>
+      </div>
+      
       <button onClick={props.nextWord}>Skip</button>
       <button onClick={props.increaseScore}>Correct</button>
-      <PlayerCamera />
+      
     </div>
   )
 }
