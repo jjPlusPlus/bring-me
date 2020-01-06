4579ad127bc73fc09ef9ec1c14422a4a1cde1565
JJ <justinjcode@gmail.com>
2020-01-06 14:07:22 -0800

    Add the Match route

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ import {
 
 import GameWrapper from "./components/GameWrapper";
 import Lobby from "./components/Lobby";
+import Match from "./components/Match";
 
 Amplify.configure(awsconfig);
 Amplify.addPluggable(new AmazonAIPredictionsProvider());
@@ -23,6 +24,9 @@ const App = () => {
     <Switch>
       <Route path="/game">
         <GameWrapper />
+      </Route>
+      <Route path="/match/:channel/:port">
+        <Match />
       </Route>
       <Route path="/">
         <Lobby />
